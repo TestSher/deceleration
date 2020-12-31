ffe63ef151c396db16cb20f4176ae69851c47cb4
TheSher <76001218+TheSher@users.noreply.github.com>
2020-12-31 15:29:42 +0200

    Add files via upload

--- Simple_gauge.py
+++ Simple_gauge.py
@@ -2,47 +2,74 @@ from tkinter import *
 import math
 import PIL.Image, PIL.ImageTk
 import my_serial as ser
-global resault
+global result
+result=0
+
 
 def rotate_pointer(degree):
-    global my_line,my_line1
+    global my_line , my_line1
 
     if my_line != None:
         my_canvas.delete(my_line)
         my_canvas.delete(my_line1)
+
         my_line = None
         my_line1 = None
+
         
 
     x = 200 * math.cos(((2 * math.pi) / 360.0) * degree) + 244
     y = 255 - 200 * math.sin(((2 * math.pi) / 360.0) * degree)
     my_line = my_canvas.create_line(244, 255, x, y, fill='black', width=10)
     my_line1 = my_canvas.create_line(244, 255, x, y, fill='white', width=3)
+    
 
 #==========================================================================
-resault=0
+avg_buff = [0] * 10
+index = 0
+avg_samples = 0
+def avg_filter(sample):
+    global avg_samples, index
+    
+    weight_sample = sample / 10
+    avg_samples = avg_samples - avg_buff[index] + weight_sample
+    avg_buff[index] = weight_sample
 
-def f():
+    index += 1
+    if index >= 10:
+        index = 0
 
+    return avg_samples
+
+
+#==========================================================================
+def needle():
     msg = ser.get_message(ser1)
     if msg != None:
         msg1 = msg[1:-1]
         result = ser.parse_message(msg1)
-        #print(resault)
-        print(result[0])
-        rotate_pointer(result[0]*300)
+        avg_result = avg_filter(result[0])
+        print(avg_result)
+        rotate_pointer(avg_result*294.2)
 
-    root.after(30, f)
+
+    root.after(30, needle)
 
 ser1 = ser.my_init_serial()
+
+
+    
 
 #==========================================================================
 root = Tk()
 root.title('Gauge')
-root.geometry('480x350+0+0')
+root.geometry('480x350+900+600')
 root.resizable(width=False,height=False)
 my_canvas = Canvas(root, width=475, height=475, bg='white')
 my_canvas.pack(pady=0)
+
+photo = PhotoImage(file ="images/matmon.ico")
+root.iconphoto(False, photo)
 
 my_line = my_canvas.create_line(250, 250, 250, 250, fill='red', width=10)
 my_line1 = my_canvas.create_line(250, 250, 250, 250, fill='red', width=10)
@@ -51,7 +78,7 @@ image = PIL.ImageTk.PhotoImage(image=img)
 my_image = my_canvas.create_image(2, 2, anchor=NW, image=image)
 
 
-root.after(1000, f)
+root.after(1000, needle)
 
 
 root.mainloop()
